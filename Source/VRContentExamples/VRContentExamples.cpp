029ac43c4665726bff4bc65fa47b36d8a4bb1c74
eric o <i.synapse@gmail.com>
2017-07-16 20:49:38 -0400

    S1
    D1

---
+++ Source/VRContentExamples/VRContentExamples.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "VRContentExamples.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, VRContentExamples, "VRContentExamples" );
